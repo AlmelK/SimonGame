e37171a920bcd58a74c419370ff31b2f73d5db8d
AlmelK <af5909704@gmail.com>
2022-04-15 00:02:38 +0700

    add sound for buttons

--- src/components/SimonGame.vue
+++ src/components/SimonGame.vue
@@ -1,13 +1,29 @@
 <template>
 <div class="root">
+
+<audio src="../assets/sounds/1.mp3" ref="sound1"></audio>
+<audio src="../assets/sounds/2.mp3" ref="sound2"></audio>
+<audio src="../assets/sounds/3.mp3" ref="sound3"></audio>
+<audio src="../assets/sounds/4.mp3" ref="sound4"></audio>
+
 <div class="game-content">
     <div class="btn-container">
-
-        <button id="yellow"></button>
-        <button id="green"></button>
-        <button id="red"></button>
-        <button id="blue"></button>
-
+        <div class="button" 
+        id="yellow" 
+        :class="{highlight: hlYellow}"
+        @click="userInput(3)"></div>
+        <div class="button" 
+        id="green" 
+        :class="{highlight: hlGreen}"
+        @click="userInput(2)"></div>
+        <div class="button" 
+        id="red" 
+        :class="{highlight: hlRed}"
+        @click="userInput(1)"></div>
+        <div class="button" 
+        id="blue" 
+        :class="{highlight: hlBlue}"
+        @click="userInput(4)"></div>
     </div>
     <div class="game-settings">
       <div class="choose-level">
@@ -26,7 +42,7 @@
       </div>
 
       <div class="start-game">
-        <button>Start</button>
+        <button @click="playTone(1)">Start</button>
       </div>
     </div>
 
@@ -38,9 +54,100 @@
 export default {
   data() {
     return {
-      difficulty: "",
+      difficulty: "easy",
+      started: false, //did the game start
+      gamePlay: false, //check if the game make a move
+      allowInput: true,//allow user input
+
+      hlRed: false, //highlight for red button when it is in focus
+      hlYellow: false, //highlight for yellow button when it is in focus
+      hlGreen: false,//highlight for green button when it is in focus
+      hlBlue: false, //highlight for blue button when it is in focus
+
+      count: 0,
+      roundKol: 20,
+      
+      series: [],
+      userInputs: [],
     };
   },
+  computed: {
+    //Show current count
+    showCount() {
+      if (this.count == 0) 
+        return "0"
+      else
+        return this.count
+    }
+  },
+  methods:{
+    //for random sound selection
+    randomTone() {
+      return Math.floor(Math.random() * 4) + 1
+    },
+    //start of the game
+    start() {
+      if (this.count == 0) {
+        this.started = true //game is started
+        this.addTone() //add tone to game series
+        this.playSeries() //play game series
+      }
+    },
+    addTone() {
+      this.count++
+      this.series.push(this.randomTone())
+    },
+    playSeries() {
+      let self = this
+      let delay = 1000
+      this.series.forEach(function(tone, index, array) {
+        if(index == array.length - 1)
+          setTimeout(function() {
+            if (self.started) {
+              self.playTone(tone)
+            }
+          }, delay)
+          else
+            setTimeout(function() {self.playTone(tone)}, delay)
+          delay += 1000
+      })      
+    },
+    userInput(tone){
+      if(!this.allowInput)
+        return
+      this.playTone(tone)
+      this.userInputs.push(tone)
+    },
+    playTone(tone){
+      switch(tone) {
+        case 1:
+          this.$refs.sound1.pause()
+          this.$refs.sound1.currentTime = 0
+          this.$refs.sound1.play()
+          this.hlRed = true
+          break;
+        case 2:
+          this.$refs.sound2.pause()
+          this.$refs.sound2.currentTime = 0
+          this.$refs.sound2.play()
+          this.hlGreen = true
+          break;
+        case 3:
+          this.$refs.sound3.pause()
+          this.$refs.sound3.currentTime = 0
+          this.$refs.sound3.play()
+          this.hlYellow = true
+          break;
+        case 4:
+          this.$refs.sound4.pause()
+          this.$refs.sound4.currentTime = 0
+          this.$refs.sound4.play()
+          this.hlBlue = true
+          break;
+      }
+    }
+  }
+  
 };
 </script>
 <style scoped lang="sass">
@@ -66,7 +173,7 @@ export default {
         margin-right: 40px
         margin-left: 40px
 
-        button
+        .button
             width: 100px
             height: 100px
             border: none
@@ -75,30 +182,26 @@ export default {
         #red
             background: #ff5050     
             
-            &:hover
-                box-shadow: 1px 1px 2px 3px red
-                transition: .2s     
+            .highlight
+                background: lighten(red, 5%)   
 
         #green
             background: #6f9
 
-            &:hover
-                box-shadow: 1px 1px 2px 3px green
-                transition: .2s 
+            .highlight
+                background: lighten(green, 5%)
 
         #yellow
             background: #ffff44
 
-            &:hover
-                box-shadow: 1px 1px 2px 3px yellow
-                transition: .2s 
+            .highlight
+                background: lighten(#ffd700, 5%)
 
         #blue
             background: #00ccff
 
-            &:hover
-                box-shadow: 1px 1px 2px 3px blue
-                transition: .2s 
+            .highlight
+                background: lighten(#0000cd, 5%)
   .game-settings
       
       .choose-level
